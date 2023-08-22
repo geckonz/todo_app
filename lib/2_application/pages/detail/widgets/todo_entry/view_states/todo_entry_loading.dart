e0e6487aa75a63f557964f4eed10bbc835c48469
Bryce Watkins <bwatkinsnz@gmail.com>
2023-08-23 05:39:11 +1200

    task t7

--- lib/2_application/pages/detail/widgets/todo_entry/view_states/todo_entry_loading.dart
+++ lib/2_application/pages/detail/widgets/todo_entry/view_states/todo_entry_loading.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer_animation/shimmer_animation.dart';
 
 class ToDoEntryLoading extends StatelessWidget {
   const ToDoEntryLoading({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator.adaptive(),
-      );
+    return Shimmer(
+      color: Colors.black,
+      child: Card(
+        color: Colors.grey.withOpacity(0.8),
+        margin: const EdgeInsets.all(0),
+        child: Container(
+          constraints: BoxConstraints(
+              minHeight: 100,
+              minWidth: MediaQuery.of(context).size.width * 0.9),
+        ),
+      ),
+    );
   }
 }
